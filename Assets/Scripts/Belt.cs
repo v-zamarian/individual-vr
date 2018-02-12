4edddb6d1a62bd23707b849951d660d91dd337ab
Victor Zamarian <v_zamarian@u.pacific.edu>
2018-02-11 16:31:58 -0800

    Added base conveyor belt prefab.

---
+++ Assets/Scripts/Belt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Belt : MonoBehaviour {
+
+    public float force;
+    float start;
+
+	// Use this for initialization
+	void Start () {
+        start = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /* increase the speed over time
+        float current = Time.time;
+
+        if (current - start > 5.0) {
+            force += 2;
+            start = current;
+        }*/
+
+        transform.GetComponent<Rigidbody>().velocity = force * transform.right;
+        
+	}
+}
