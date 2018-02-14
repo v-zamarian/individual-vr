37a6a533ae75c6ba17903c9e21ac932ba353d24b
Victor Zamarian <v_zamarian@u.pacific.edu>
2018-02-14 13:33:58 -0800

    Tracking other scripts.

---
+++ Assets/Scripts/BeltController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltController : MonoBehaviour {
+
+    public float speed; //public for testing
+
+    float start;
+
+    // Use this for initialization
+    void Start () {
+        start = Time.time;
+        //speed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //increase the speed over time
+        /*float current = Time.time;
+
+        if (current - start > 5.0) {
+            speed += 1;
+            start = current;
+        }*/
+    }
+}
