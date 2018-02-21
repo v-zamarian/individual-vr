d0feff2016975b717230bb7139fc4ce70957c0e0
Victor Zamarian <v_zamarian@u.pacific.edu>
2018-02-21 13:44:02 -0800

    Updated ItemTrigger and modified scene a bit.

--- Assets/Scripts/ItemTrigger.cs
+++ Assets/Scripts/ItemTrigger.cs
@@ -8,12 +8,7 @@ using UnityEngine;
 public class ItemTrigger : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer == LayerMask.NameToLayer("Object")) {
-            
-            if (other.gameObject.transform.parent != null) {
-                Destroy(other.gameObject.transform.parent.gameObject);
-            } else {
-                Destroy(other.gameObject);
-            }
+            Destroy(other.gameObject);
         }
     }
 }
